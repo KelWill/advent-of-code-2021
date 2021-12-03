b1826064553acf211b4fc070d6a06dabbe458764
will <will@classdojo.com>
2021-12-03 19:05:42 +0000

    day3 rewrite

--- day3.js
+++ day3.js
@@ -1,17 +1,11 @@
 const fs = require("fs");
 const _ = require("lodash");
-const inputString = fs.readFileSync("./day3input")
+const realInput = fs.readFileSync("./day3input")
   .toString();
 
-function toInput (s) {
-  return s.split("\n")
-  .map((s) => {
-    return s.split("").map((n) => parseInt(n, 10))
-  });
-
-}
-  
-const input = toInput( inputString || `10110
+const testInput = `00100
+11110
+10110
 10111
 10101
 01111
@@ -20,70 +14,36 @@ const input = toInput( inputString || `10110
 10000
 11001
 00010
-01010`);
+01010`;
 
-
-function part1() {
-  const valueCounts = getValueCounts();
-  const gamma = parseInt(valueCounts.map((v) => {
-    return (v["0"] > v["1"]) ? "0" : "1";
-  }).join(""), 2);
-
-  const epsilon = parseInt(valueCounts.map((v) => {
-    return (v["1"] > v["0"]) ? "0" : "1";
-  }).join(""), 2);
-  console.log(gamma);
-  console.log(epsilon);
-  console.log(gamma * epsilon);
+function part1 () {
+  const rows = realInput.split("\n")
+  const countOfOnes = _.range(0, rows[0].length)
+    .map((i) => _.filter(rows, r => r[i] === "1").length)
+  const gamma = parseInt(countOfOnes.map((c) => c > rows.length - c ? 1 : 0).join(""), 2);
+  const epsilon = parseInt(countOfOnes.map((c) => c <= rows.length - c ? 1 : 0).join(""), 2);
+  console.log(gamma, epsilon, gamma * epsilon);
 }
 
+part1();
 
-function getValueCounts (input) {
-  const valueCounts = [];
-  for (let i = 0; i < input[0].length; i++) {
-    for (let j = 0; j < input.length; j++) {
-      valueCounts[i] = valueCounts[i] || {0: 0, 1: 0};
-      valueCounts[i][input[j][i]]++;
-    }
-  }
-  return valueCounts;
+function filterRows (rows, chooseChar, i = 0) {
+  if (rows.length === 1) return rows[0];
+  const oneCount = rows.filter((r) => r[i] === "1").length;
+  const mostCommon = oneCount >= rows.length - oneCount ? "1" : "0";
+  return filterRows(
+    rows.filter(r => r[i] === chooseChar(mostCommon)),
+    chooseChar,
+    i + 1,
+  )
 }
 
-
-
-function part2 () {
-  let i = 0;
-  let currInput = input;
-  while (currInput.length > 1) {
-    const valueCounts = getValueCounts(currInput);
-    const mostCommonBit = valueCounts[i]["0"] > valueCounts[i]["1"] ? 0 : 1;
-    
-    currInput = currInput.filter((row) => {
-      return row[i] === mostCommonBit;
-    });
-    i++;
-  }
-
-
-  const oxygen = parseInt(currInput[0].join(""), 2);
-
-
-  // If 0 and 1 are equally common, keep values with a 0 in the position being considered
-  i = 0;
-  currInput = input;
-  while (currInput.length > 1) {
-    const valueCounts = getValueCounts(currInput);
-    const mostCommonBit = valueCounts[i]["0"] <= valueCounts[i]["1"] ? 0 : 1;
-    
-    currInput = currInput.filter((row) => {
-      return row[i] === mostCommonBit;
-    });
-    i++;
-  }
-
-  const c02 = parseInt(currInput[0].join(""), 2);
+const rows = realInput.split("\n");
+const c = filterRows(rows, (c) => c === "1" ? "1" : "0");
+const o = filterRows(rows, (c) => c === "1" ? "0" : "1");
+console.log(
+  parseInt(c, 2) * parseInt(o,2 )
+);
   
-  console.log(oxygen, c02, oxygen * c02);
-}
 
-part2();
+
