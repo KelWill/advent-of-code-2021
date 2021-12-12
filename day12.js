ae2f889cca4a607a09072618db2151d511183754
will <will@classdojo.com>
2021-12-12 14:25:39 -0800

    more performance?

--- day12.js
+++ day12.js
@@ -1,51 +1,76 @@
-
 function p1 (input) {
-  const nodes = input.split("\n").reduce((acc, row) => {
-    const [start, end] = row.split("-")
-    acc[start] = acc[start] || []
-    acc[end] = acc[end] || [];
-    acc[start].push(end);
-    acc[end].push(start);
-    return acc;
-  }, {});
+  // mapping keys -> numbers so that we can use arrays to track relationships
+  const map = {};
 
-  let paths = [["start", {}, false]];
+  let i = 0;
+  const lowercaseIndexes = [];
+  const nodes = input.split("\n").reduce((acc, row) => {
+    const [a, b] = row.split("-");
+
+
+    const aIndex = map[a] = map[a] != null ? map[a] : i++;
+    const bIndex = map[b] = map[b] != null ? map[b] : i++;
+
+  
+    if (a === a.toLowerCase()) {
+      lowercaseIndexes[aIndex] = 1;
+    }
+    if (b === b.toLowerCase()) {
+      lowercaseIndexes[bIndex] = 1;
+    }
+
+    acc[aIndex] = acc[aIndex] || []
+    acc[bIndex] = acc[bIndex] || [];
+
+    acc[aIndex].push(bIndex);
+    acc[bIndex].push(aIndex);
+
+    return acc;
+  }, []);
+
   let completedCount = 0;
+
+  const startIndex = map.start;
+  const endIndex = map.end;
+  const paths = [[startIndex, [], false]];
 
   let curr;
   while (curr = paths.pop()) {
-
-    const [ currentKey, visitedLowers, hasDoubledVisitedLower ] = curr;
-    const currentNode = nodes[currentKey];
-    for (const k of currentNode) {
-      if (k === "start") continue;
-      if (k === "end") {
+    const [ i, seen, doubleVisited ] = curr;
+    for (const k of nodes[i]) {
+      if (k === startIndex) continue;
+      if (k === endIndex) {
         completedCount++;
         continue;
       }
 
-      if (k === k.toLowerCase()) {
-        if (!visitedLowers[k]) {
-          paths.push([
-            k,
-            { ...visitedLowers, [k]: true },
-            hasDoubledVisitedLower,
-          ]);
-          continue;
-        }
-
-        if (hasDoubledVisitedLower) continue;
+      if (!lowercaseIndexes[k]) {
         paths.push([
-          k, visitedLowers, k
-        ])
+          k,
+          seen,
+          doubleVisited,
+        ]);
         continue;
       }
 
+      if (!seen[k]) {
+        const n = seen.slice();
+        n[k] = true;
+
+        paths.push([
+          k,
+          n,
+          doubleVisited,
+        ]);
+        continue;
+      }
+
+      if (doubleVisited) continue;
+
       paths.push([
-        k,
-        visitedLowers,
-        hasDoubledVisitedLower,
-      ]);
+        k, seen, true
+      ])
+      continue;
     }
 
   }
