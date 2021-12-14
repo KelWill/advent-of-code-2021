6a5b6429e63a36648156f6428ead5452d87464ef
will <will@classdojo.com>
2021-12-14 21:37:53 +0000

    day14 recursive

--- day14.js
+++ day14.js
@@ -63,9 +63,56 @@ function p (input, n) {
   return _.last(values) - _.first(values);
 }
 
+
+function increment (o, k, n=1) {
+  o[k] = o[k] || 0;
+  o[k] += n;
+}
+function pr (input, n) {
+  const [start, rules] = input.split("\n\n")
+  const m = {};
+  for (const rule of rules.split("\n")) {
+    const [from, to] = rule.split(" -> ");
+    m[from] = to;
+  }
+
+  const pairs = {};
+  for (let i = 0; i < start.length - 1; i++) {
+    increment(pairs, start[i] + start[i+1]);
+  }
+
+  function getPairCounts (pairs, n) {
+    if (n === 0) return pairs;
+    
+    const updated = {};
+    for (const [pair, score] of Object.entries(pairs)) {
+      const [c1, c2] = pair.split("");
+      increment(updated, c1 + m[pair], score)
+      increment(updated, m[pair] + c2, score);
+    }
+
+    return getPairCounts(updated, n - 1);
+
+  }
+  const counts = getPairCounts(pairs, n);
+  const countsByCharacter = {};
+  for (const [pair, score] of Object.entries(counts)) {
+    const [k] = pair.split("");
+    increment(countsByCharacter, k, score);
+  }
+  increment(countsByCharacter, start[start.length - 1], 1);
+
+  const cs = _.sortBy(Object.values(countsByCharacter));
+  console.log(cs);
+  return _.last(cs) - _.first(cs);
+}
+
 console.log(
   p(testInput, 10) === 1588,
   p(realInput, 10) === 2194,
   p(testInput, 40) === 2188189693529,
+  pr(realInput, 40) === p(realInput, 40),
   p(realInput, 40),
+  pr(testInput, 10),
+
 )
